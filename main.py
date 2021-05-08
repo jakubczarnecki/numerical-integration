833fca7b9491dd1429c14cd8392a426d4fda7c72
229935 <229935@edu.p.lodz.pl>
2021-05-08 14:38:55 +0200

    simpson ale do poprawy

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import numpy as np
+import functions as f
+from simpson import advancedSimpson
 
 if __name__ == '__main__':
     print("Metody numeryczne - zadanie 4. Całkownanie numeryczne\n")
     print("Wariant 2 - wielomiany Hermite'a\n")
+
+    print(advancedSimpson(f.f1, -5, 5, 3, 5))
--- simpson.py
+++ simpson.py
@@ -1,8 +1,30 @@
 import numpy as np
 
-def simpson(f, a, b, eps):
-    dx = (b - a) / eps
-    x = np.linspace(a, b, eps+1)
-    y = f(x)
-    result = dx / 3 * np.sum(y[0:-1:2] + 4 * y[1::2] + y[2::2])
-    return result
+def simpson(f, a, b):
+    av = (b - a) / 2
+    integral = (np.exp((-a) ** 2) * f(a)
+                + 4 * np.exp((- (a + b) / 2)  ** 2) * f((a + b) / 2)
+                + (np.exp((-b) ** 2) * f(b))) * av / 3
+    return integral
+
+def advancedSimpson(f, a, b, n, epsilon):
+    integral = simpson(f, a, b)
+    isUnsatisfying = True
+    while isUnsatisfying:
+        temp = 0
+        av = (b - a) / (2 * n)
+        left = a
+        right = a + 2 * av
+        for i in range (n):
+            i = simpson(f, left, right)
+            temp += i
+            left = right
+            right += 2 * av
+            print(i)
+        if abs(temp - integral) < epsilon:
+            isUnsatisfying = False
+            integral = temp
+        else:
+            integral = temp
+            n += 1
+    return integral
